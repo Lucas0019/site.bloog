864b8a6ff764f2c3d5f243ca81ab21d80626ba88
Lucas Xavier <lxvier.00@outlook.com>
2022-10-08 18:42:30 -0300

    feat(component): implemented Header and ActiveLink component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
 import '../styles/global.scss'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header/>
       <Component {...pageProps} />
     </>
   )
